75732d103c6083288c2abb6d0240463030126379
Prueba SCS <scsprueba48@gmail.com>
2025-11-05 20:50:05 +0000

    pues no, en la web me sale Backend Not Found Why am I seeing this? There

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,13 +1,17 @@
+'use server';
+
 import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/app';
 import { getFirestore as getFirestoreSdk } from 'firebase/firestore';
 
+// This is a hardcoded config object.
+// In a real-world scenario, this should be loaded from environment variables.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCCozUn2lAcvVM6VUmSFlnkLnLdP1jJVnU',
+  authDomain: 'studio-6792195927-50f25.firebaseapp.com',
+  projectId: 'studio-6792195927-50f25',
+  storageBucket: 'studio-6792195927-50f25.appspot.com',
+  messagingSenderId: '835504863875',
+  appId: '1:835504863875:web:240adbdea3fa388b57b9b6',
 };
 
 function getFirebaseApp() {
